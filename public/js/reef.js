55d6ced2a898142ae754125e5cfbfa027b698013
GetOfMyLAN <jackpsthefirst@gmail.com>
2017-07-29 16:55:06 +1000

    k

--- public/js/reef.js
+++ public/js/reef.js
@@ -1,15 +1,24 @@
+<<<<<<< HEAD
+//GETTING DATA
+$.get('/python/data/waveData.json', {
+}, function (data) {
+  console.log(data);
+}, 'json');
+=======
 //GETTINF DATA
- 
+>>>>>>> fdb64095a3aad2441064532010cd2cf0d825b29f
 
-//DATA AND VARIBELS
-var counter = 0;
-var labels = [];
-var type = [];
-var data = [];
-var colors = [];
-var label = [];
-var numofpanels = 0;
+$(document).ready(function () {
+  //DATA AND VARIBELS
+  var counter = 0;
+  var labels = [];
+  var type = [];
+  var data = [];
+  var colors = [];
+  var label = [];
+  var numofpanels = 0;
 
+<<<<<<< HEAD
 //CLICKS
 var chartl = $("#right");
 chartl.click(function () {
@@ -17,9 +26,10 @@ chartl.click(function () {
   if (counter > numofpanels) {
     counter = 0
   };
-  chartl.slideUp(1000, function(){
-  graph();
-  chartl.slideDown(1000)})
+  chartl.slideUp(1000, function () {
+    graph();
+    chartl.slideDown(1000)
+  })
 });
 var chartr = $("#left");
 chartr.click(function () {
@@ -27,34 +37,63 @@ chartr.click(function () {
   if (counter < 0) {
     counter = numofpanels;
   };
-  chartr.slideUp(1000, function(){
-  graph();
-  chartr.slideDown(1000)})
+  chartr.slideUp(1000, function () {
+    graph();
+    chartr.slideDown(1000)
+  })
 });
-
-//GRPAHING FUNCTION
-function graph() {
-  var ctx = $("#chart").get();
-  var mono = new Chart(ctx, {
-    type: type[counter],
-    data: {
-      labels: labels[counter],
-      datasets: [{
-        label: label[counter],
-        data: data[counter],
-        backgroundColor: colors[counter],
-        borderColor: colors[counter],
-        borderWidth: 1
-          }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true,
-          }
-              }]
-      }
-    }
+=======
+  //CLICKS
+  var chartl = $("#right");
+  chartl.click(function () {
+    counter++
+    if (counter > numofpanels) {
+      counter = 0
+    };
+    chartl.slideUp(1000, function () {
+      graph();
+      chartl.slideDown(1000)
+    })
   });
-};
+  var chartr = $("#left");
+  chartr.click(function () {
+    counter = counter - 1;
+    if (counter < 0) {
+      counter = numofpanels;
+    };
+    chartr.slideUp(1000, function () {
+      graph();
+      chartr.slideDown(1000)
+    })
+  });
+>>>>>>> fdb64095a3aad2441064532010cd2cf0d825b29f
+
+  //GRPAHING FUNCTION
+  function graph() {
+    var ctx = $("#chart").get();
+    var mono = new Chart(ctx, {
+      type: type[counter],
+      data: {
+        labels: labels[counter],
+        datasets: [{
+          label: label[counter],
+          data: data[counter],
+          backgroundColor: colors[counter],
+          borderColor: colors[counter],
+          borderWidth: 1
+          }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+            }
+              }]
+        }
+      }
+    });
+  };
+
+
+});

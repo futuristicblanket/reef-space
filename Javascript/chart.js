e5aeb02bdc94aa1311a6ff8e0896fa26c9f37c9e
GetOfMyLAN <jackpsthefirst@gmail.com>
2017-07-29 12:56:58 +1000

    Adding everything and files

---
+++ Javascript/chart.js
@@ -0,0 +1,57 @@
+//DATA AND VARIBELS
+var counter = 0;
+var labels = [];
+var type = [];
+var data = [];
+var colors = [];
+var label = [];
+var numofpanels = 0;
+
+//CLICKS
+var chartl = $("#right");
+chartl.click(function () {
+  counter++
+  if (counter > numofpanels) {
+    counter = 0
+  };
+  chartl.slideUp(1000, function(){
+  graph();
+  chartl.slideDown(1000)})
+});
+var chartr = $("#left");
+chartr.click(function () {
+  counter = counter - 1;
+  if (counter < 0) {
+    counter = numofpanels;
+  };
+  chartr.slideUp(1000, function(){
+  graph();
+  chartr.slideDown(1000)})
+});
+
+//GRPAHING FUNCTION
+function graph() {
+  var ctx = $("#chart").get();
+  var mono = new Chart(ctx, {
+    type: type[counter],
+    data: {
+      labels: labels[counter],
+      datasets: [{
+        label: label[counter],
+        data: data[counter],
+        backgroundColor: colors[counter],
+        borderColor: colors[counter],
+        borderWidth: 1
+          }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+          }
+              }]
+      }
+    }
+  });
+};

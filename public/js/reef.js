712153e07a125bfc47a61b5cc2c4054d467f6ccb
GetOfMyLAN <jackpsthefirst@gmail.com>
2017-07-30 11:08:36 +1000

    GRAPHS CHANGE

--- public/js/reef.js
+++ public/js/reef.js
@@ -21,51 +21,55 @@ $(document).ready(function () {
     if (counter > numofpanels) {
       counter = 0
     };
+    console.log(counter);
+    char(atype, labels, label, data, colors, counter);
   })
-});
-var chartr = $("#left");
-chartr.click(function () {
-  counter = counter - 1;
-  if (counter < 0) {
-    counter = numofpanels;
-  };
-});
-//DATA AND VARIBELS
-var counter = 0;
-var labels = [['red', 'red', 'red', 'red', 'red', 'red'], ['red', 'red', 'red', 'red', 'red', 'red']];
-var atype = ['line', 'bar'];
-var data = [[1, 2, 4, 8, 16, 32], [1, 2, 4, 8, 16, 32]];
-var colors = [['#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000'], ['#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000']];
-var label = [['red'], ['Blue']];
-var numofpanels = 1;
+  var chartr = $("#left");
+  chartr.click(function () {
+    counter = counter - 1;
+    if (counter < 0) {
+      counter = numofpanels;
+    };
+    console.log(counter);
+    char(atype, labels, label, data, colors, counter);
+  });
+  //DATA AND VARIBELS
+  var counter = 0;
+  var labels = [['red', 'red', 'red', 'red', 'red', 'red'], ['red', 'red', 'red', 'red', 'red', 'red']];
+  var atype = ['line', 'bar'];
+  var data = [[1, 2, 4, 8, 16, 32], [1, 2, 4, 8, 16, 32]];
+  var colors = [['#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000'], ['#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000', '#ff0000']];
+  var label = [['red'], ['Blue']];
+  var numofpanels = 1;
 
-//GRPAHING FUNCTION
-//function char(atype, labels, label, data, colors, counter) {
-var ctx = $("#myChart").get();
+  //GRPAHING FUNCTION
+  function char(atype, labels, label, data, colors, counter) {
+    var ctx = $("#myChart").get();
 
-var mono = new Chart(ctx, {
-  type: atype[counter],
-  data: {
-    labels: labels[counter],
-    datasets: [{
-      label: label[counter],
-      data: data[counter],
-      backgroundColor: colors[counter],
-      borderColor: colors[counter],
-      borderWidth: 1
+    var mono = new Chart(ctx, {
+      type: atype[counter],
+      data: {
+        labels: labels[counter],
+        datasets: [{
+          label: label[counter],
+          data: data[counter],
+          backgroundColor: colors[counter],
+          borderColor: colors[counter],
+          borderWidth: 1
           }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true,
-        }
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+            }
               }]
-    },
-    fill: true,
-  }
+        },
+        fill: true,
+      }
 
+    });
+  }
+  char(atype, labels, label, data, colors, counter);
 });
-//}
-//char(atype, labels, label, data, colors, counter);

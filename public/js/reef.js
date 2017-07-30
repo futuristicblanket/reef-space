8165185c749ee6a4868d16d252f1a847145a30d5
GetOfMyLAN <jackpsthefirst@gmail.com>
2017-07-30 10:21:36 +1000

    CHART

--- public/js/reef.js
+++ public/js/reef.js
@@ -40,31 +40,32 @@ var label = [['red'], ['Blue']];
 var numofpanels = 1;
 
 //GRPAHING FUNCTION
-function char(atype, labels, label, data, colors, counter) {
-  var ctx = $("#chart").get();
-  var mono = new Chart(ctx,{
-    type: atype[counter],
-    data: {
-      labels: labels[counter],
-      datasets: [{
-        label: label[counter],
-        data: data[counter],
-        backgroundColor: colors[counter],
-        borderColor: colors[counter],
-        borderWidth: 1
-          }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true,
-          }
-              }]
-      },
-      fill: true,
-    }
+//function char(atype, labels, label, data, colors, counter) {
+var ctx = $("#myChart").get();
 
-  });
-}
-char(atype, labels, label, data, colors, counter);
+var mono = new Chart(ctx, {
+  type: atype[counter],
+  data: {
+    labels: labels[counter],
+    datasets: [{
+      label: label[counter],
+      data: data[counter],
+      backgroundColor: colors[counter],
+      borderColor: colors[counter],
+      borderWidth: 1
+          }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true,
+        }
+              }]
+    },
+    fill: true,
+  }
+
+});
+//}
+//char(atype, labels, label, data, colors, counter);

31923f467d13778464e510c1fce6e51a25b9d87a
mattdocherty314 <mattdocherty314@gmail.com>
2017-07-30 12:39:17 +1000

    All Current Datasets

--- python/main.py
+++ python/main.py
@@ -3,30 +3,52 @@ import json
 import urllib
 
 #Add the JSON output files
-dataFiles = ["data/waveData.json"]
+dataFiles = ["data/waveData.json",
+						"data/soilData.json",
+						"data/waterchemistryData.json",
+						"data/faunaData.json",
+						"data/faunataxaData.json"]
 #Add the URLS to get the CSV files
-dataUrls = ["http://www.ehp.qld.gov.au/data-sets/waves/wave-7dayopdata.csv?timestamp=2017-07-29EST11-06-50?"]
+dataUrls = ["http://www.ehp.qld.gov.au/data-sets/waves/wave-7dayopdata.csv?timestamp=2017-07-29EST11-06-50?",
+					 "http://www.ehp.qld.gov.au/data-sets/aquatic-fauna-db/site-details.csv",
+					 "http://www.ehp.qld.gov.au/data-sets/aquatic-fauna-db/water-chemistry.csv",
+					 "http://www.ehp.qld.gov.au/data-sets/aquatic-fauna-db/fauna-record.csv",
+					 "http://www.ehp.qld.gov.au/data-sets/aquatic-fauna-db/fauna-taxa.csv"]
 #Add the column name of the heading of the JSON file
-dataHead = ["Site"]
+dataHead = ["Site",
+					 "Site Name",
+					 "Site ID",
+					 "Site ID",
+					 "Taxon ID"]
 #Add the column names of the data of the JSON file
-dataBody = [["DateTime", "Latitude", "Longitude", "SST",  "Hsig", "Hmax"]]
+dataBody = [["DateTime", "Latitude", "Longitude", "SST",  "Hsig", "Hmax"],
+					 ["Latitude",	"Longitude", "Elevation",	"Total depth of the bore", "Stratigraphy Sampled", "Lithology Sampled"],
+					 ["Sample Number", "Water level (mbgl)", "Temperature", "pH",	"Electrical conductivity", "Dissolved oxygen (mg/L)",	"Dissolved oxygen (%/saturation)", "Total Nitrogen", "Total organic carbon", "Total phosphorus", "Filterable reactive phosphorus", "Ammonium", "Nitrates", "Redox potential"],
+					 ["Sample Number", "Taxon ID", "Taxon Name", "Abundance"],
+					 ["Taxon Name", "Taxon Detail", "Common Name", "Stygofauna", "Stygofauna group", "Phylum", "Sub-Phylum", "Superclass", "Class", "Sub-Class", "Superorder", "Order", "Sub-Order", "Family", "Sub-family", "Genus", "Species"]]
 
 
 def main():
 	for f in range(len(dataFiles)):
 		allData = []
 		dataRows = urlToLines(dataUrls[f])
-		dataDict = rowToDictArr(dataRows)
+		if (dataFiles[f] == "data/waveData.json"):
+			dataDict = rowToDictArr(dataRows, (dataRows[1].split(', ')))
+		else:
+			dataDict = rowToDictArr(dataRows, (dataRows[0].split(',')))
 		clearFile(dataFiles[f])
 		for d in range(len(dataDict)):
-			if d > 0:
+			if (dataFiles[f] == "data/waveData.json"):
+				if (d > 0):
+					JSONDict = formatJSON(dataDict[d], dataHead[f], dataBody[f])
+					allData.append(JSONDict)
+			else:
 				JSONDict = formatJSON(dataDict[d], dataHead[f], dataBody[f])
 				allData.append(JSONDict)
 		dictToJSON(allData, dataFiles[f])
 			
-def rowToDictArr(row):
+def rowToDictArr(row, columnNames):
 	dictArr = []
-	columnNames = (row[1].split(', '))
 	for r in range(len(row)):
 		dictionary = dict(zip(columnNames, row[r].split(',')))
 		dictArr.append(dictionary)
